3a18ae93e4b0d8166f6967437ddaf8089ea29be1
Enrico Marogna <info@enricomarogna.com>
2023-01-24 23:06:48 +0100

    Main script
    - Aggiunta funzione per rinominare i files dentro l'app directory in modo sequenziale

--- main_script.sh
+++ main_script.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-# Location of the applications folder
+# Posizionati nella directory corretta
 app_folder="applicazioni"
 
-# Check if the applications folder exists
+# Verifica che la directory esista
 if [ ! -d "$app_folder" ]; then
   echo "The applications folder does not exist. Exiting script."
   exit 1
 fi
 
-# Change directory to the applications folder
+# Cambia la directory corrente
 cd "$app_folder"
 
-# Assign execute permissions to all .sh files
+# Assegna i permessi di esecuzione a tutti i file .sh
 for file in *.sh; do
   if [ ! -x "$file" ]; then
     chmod +x "$file"
   fi
 done
 
-# Execute all .sh files
+# Imposta il valore iniziale del contatore
+counter=1
+# Loop attraverso tutti i file nella directory
+for file in *_*.sh; do
+  # Ottieni il nome del file prima della prima sottolineatura
+  prefix=$(echo $file | cut -d'_' -f1)
+  # ottieni il nome del file successivo alla prima sottolineatura
+  suffix=$(echo $file | cut -d'_' -f2)
+  # Rinomina il file con il valore del contatore e il prefisso originale
+  mv $file "${counter}_${suffix}"
+  # Incrementa il contatore
+  ((counter++))
+done
+
+# Esegui tutti i file .sh nella directory
 for file in *.sh; do
   ./"$file"
 done
